fbabdf61337f9c15a6886ff428da5fcc40bfe4a7
Anthony Datu <adatu@hp.com>
2016-06-15 14:01:13 -0700

    first commit with bootstrap

---
+++ wp-content/themes/hermitto/index.php
@@ -0,0 +1,7 @@
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12"> col-md-12 </div>
+		<div class="col-md-9"> col-md-9 </div>
+		<div class="col-md-3"> col-md-3 </div>
+	</div>
+</div>
