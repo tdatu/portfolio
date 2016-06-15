da87822d2e2963750d829ffc6d8b3bf312e77d62
Anthony Datu <adatu@hp.com>
2016-06-15 14:36:16 -0700

    add header function

--- wp-content/themes/hermitto/index.php
+++ wp-content/themes/hermitto/index.php
@@ -1,3 +1,14 @@
+<?php
+/**
+ * The template for displaying image attachments
+ *
+ * @package WordPress
+ * @subpackage hermitto
+ * @since hermitto 1.0
+ */
+
+get_header(); ?>
+
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-12"> col-md-12 </div>
