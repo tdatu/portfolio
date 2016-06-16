aaeb04dde9221302e4dfd0263cfbe1cefcf81ed8
Anthony Datu <adatu@hp.com>
2016-06-15 18:07:12 -0700

    fix

--- wp-content/themes/hermitto/index.php
+++ wp-content/themes/hermitto/index.php
@@ -28,34 +28,38 @@ get_header(); ?>
 
 	<div class="col-lg-9 col-xs-9 col-md-9">
 
-		<div class="row">
+		<div class="col-lg-12 col-xs-12 col-md-9">
 
-			<div class="col-lg-8 col-md-8 col-xs-12">
-				<div class="panel panel-warning">
-					<div class="panel-heading">Panel heading without title</div>
-					<div class="panel-body">
-						Basic panel example
+			<div class="row">
+
+				<div class="col-lg-8 col-md-8 col-xs-12">
+					<div class="panel panel-warning">
+						<div class="panel-heading">Panel heading without title</div>
+						<div class="panel-body">
+							Basic panel example
+						</div>
 					</div>
 				</div>
+
 			</div>
 
-		</div>
 
-
-		<div class="row">
-		
-			<div class="col-lg-8 col-md-8 col-xs-12">
-				<div class="panel panel-warning">
-					<div class="panel-heading">Panel heading without title</div>
-					<div class="panel-body">
-						Basic panel example
+			<div class="row">
+			
+				<div class="col-lg-8 col-md-8 col-xs-12">
+					<div class="panel panel-warning">
+						<div class="panel-heading">Panel heading without title</div>
+						<div class="panel-body">
+							Basic panel example
+						</div>
 					</div>
 				</div>
+
 			</div>
 
 		</div>
 	
-		<div class="col-lg-4 col-md-4 col-xs-12">
+		<div class="col-lg-3 col-md-3 col-xs-12">
 			<div class="panel panel-warning">
 				<div class="panel-heading">Twitter feeds</div>
 				<div class="panel-body">
