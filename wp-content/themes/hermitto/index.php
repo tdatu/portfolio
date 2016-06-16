b01d08b7149e661bb3cd02ca520113233695f32a
Anthony Datu <adatu@hp.com>
2016-06-15 17:56:25 -0700

    restructure

--- wp-content/themes/hermitto/index.php
+++ wp-content/themes/hermitto/index.php
@@ -23,28 +23,36 @@ get_header(); ?>
 		</div>
 
 	</div>
-	
-	<div class="row">
-	
-		<div class="col-lg-12 col-xs-12 col-md-12"> 
-			<div class="col-lg-12 col-xs-12 col-md-12"> 
-				<div class="col-lg-12 col-xs-12 col-md-12"> 
-					<img src="http://hermitto.herokuapp.com/wp-content/uploads/2016/06/photo1.jpg" class="img-circle pull right">
-				</div>
-			</div>
-		</div>
 
-	</div>
 
 
 	<div class="col-lg-9 col-xs-9 col-md-9">
-		<div class="col-lg-8 col-md-8 col-xs-12">
-			<div class="panel panel-warning">
-				<div class="panel-heading">Panel heading without title</div>
-				<div class="panel-body">
-					Basic panel example
+
+		<div class="row">
+
+			<div class="col-lg-8 col-md-8 col-xs-12">
+				<div class="panel panel-warning">
+					<div class="panel-heading">Panel heading without title</div>
+					<div class="panel-body">
+						Basic panel example
+					</div>
 				</div>
 			</div>
+
+		</div>
+
+
+		<div class="row">
+		
+			<div class="col-lg-8 col-md-8 col-xs-12">
+				<div class="panel panel-warning">
+					<div class="panel-heading">Panel heading without title</div>
+					<div class="panel-body">
+						Basic panel example
+					</div>
+				</div>
+			</div>
+
 		</div>
 	
 		<div class="col-lg-4 col-md-4 col-xs-12">
