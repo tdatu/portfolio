27ac748fbef347bc12c9923a33055cc5374f4c38
tdatu <atdatu@gmail.com>
2016-06-16 11:25:44 -0700

    update

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'hermitto');
+define('DB_NAME', 'dbname');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'username');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'sanj0s3#!');
+define('DB_PASSWORD', 'pass');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
